e912e35d0fbbe6e610d8b922eee7e1e36b60c6a4
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-14 10:31:10 +0600

    dynamic dummy website

--- another/app.js
+++ another/app.js
@@ -1,2 +1,23 @@
+/*
 const myBtnOne = document.getElementById('myBtn1');
-myBtnOne.addEventListener('click');
+myBtnOne.addEventListener('click', colorChange);
+
+function colorChange(){
+    document.body.style.backgroundColor = '#272829';
+}
+*/
+
+
+
+/*const myBtnOne = document.getElementById('myBtn1');
+myBtnOne.addEventListener('click', function colorChange(){
+    document.body.style.backgroundColor = '#272829';
+});*/
+
+
+document.getElementById('myBtn1').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#272829';
+    document.getElementById('peraText').style.color = 'white';
+    document.getElementById('titleTag').style.color = 'white';
+})
+
